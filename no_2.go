acb39b7e5aa83e02362590b22ebac08a65419a26
BagasSaputra09 <bagaszor@gmail.com>
2025-04-25 05:57:41 +0700

    Add files via upload

---
+++ no_2.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+    var a float64 = 11
+    var b float64 = 5
+    var result float64 = a + b
+
+    fmt.Printf("Outputnya = %.2f\n", result)
+}
---
+++ no_5.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+func main () {
+var x string
+x = "bagas"
+x="saputra"
+fmt.Println ("My name is", x)
+fmt. Println ("My last name is", x)
+}
